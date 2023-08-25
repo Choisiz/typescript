82e51dbd28ce6f2a7698ab85db693bff5963f04e
KyungYeul Choi <dnjsvltm201311681@gmail.com>
2023-08-25 17:34:10 +0900

    프로젝트-video컴포넌트

---
+++ Motion/dist/components/page/item/todo.js
@@ -0,0 +1,14 @@
+import { BaseComponent } from "../../component.js";
+export class TodoComponent extends BaseComponent {
+    constructor(title, todo) {
+        super(`<section class="todo">
+            <h2 class="todo__title"></h2>
+            <input type="checkbox" class="todo-checkbox"></input>
+           </section>`);
+        const titleElement = this.element.querySelector(".todo__title");
+        titleElement.textContent = title;
+        console.log(title);
+        const todoElement = this.element.querySelector(".todo-checkbox");
+        todoElement.textContent = todo;
+    }
+}
